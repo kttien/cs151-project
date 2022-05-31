3a231afd284d811a9726ee095596e84541e385d2
Kevin Tien <kevin.tien@benchling.com>
2022-05-30 22:16:10 -0700

    cleanup

--- generate_random_board.py
+++ generate_random_board.py
@@ -5,8 +5,8 @@ from random import randint
 mines = set()
 i = 0
 while len(mines) < 10:
-    mines.add((len(mines) + 1, len(mines) + 1))
-    # mines.add((randint(1, 9), randint(1, 9)))
+    # mines.add((len(mines) + 1, len(mines) + 1))
+    mines.add((randint(1, 9), randint(1, 9)))
 for i in range(1, 10):
     for j in range(1, 10):
         if (i, j) in mines:
